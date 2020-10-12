bcfcf0ef4df07d32e8bbce1fc6e4922804fd5696
TanDC_VM <tandc_vm@novaon.dc>
2020-10-12 15:35:12 +0700

    Updates

---
+++ precache-manifest.741a6b1509d2430ff988eb7e65802738.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b29d9ef65d1973e9b63e00cb7e5936",
+    "url": "/job-search-platform/index.html"
+  },
+  {
+    "revision": "5a01167ee35a9510c690",
+    "url": "/job-search-platform/static/css/main.9c56542c.chunk.css"
+  },
+  {
+    "revision": "487d83207df64c590e66",
+    "url": "/job-search-platform/static/js/2.236649ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/job-search-platform/static/js/2.236649ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a01167ee35a9510c690",
+    "url": "/job-search-platform/static/js/main.1a431f87.chunk.js"
+  },
+  {
+    "revision": "a28e636a1e5b52ead76d",
+    "url": "/job-search-platform/static/js/runtime-main.e2d563a2.js"
+  }
+]);
